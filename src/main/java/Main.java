028612b569e8b055bc462e44d291ba806dac6c7c
bodoque007 <sebastiansarco20@gmail.com>
2024-04-15 10:10:05 -0300

    stage 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,6 +15,12 @@ public class Main {
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
+      DataOutputStream clientOutput = new DataOutputStream(clientSocket.getOutputStream());
+      BufferedReader data = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String message = data.readLine();
+      String messageToClient = "HTTP/1.1 200 OK\\r\\n\\r\\n";
+      clientOutput.writeUTF(messageToClient);
+      clientOutput.flush();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
